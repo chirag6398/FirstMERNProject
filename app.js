4c371b4987871b558a80f2ad32bf809c51a6410a
Chirag <agarwalchirag112@gmail.com>
2021-04-04 13:37:28 +0530

    added dotenv module for privacy and make code structure better then before

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 
-// database
-const mongoose = require("mongoose");
-const DB =
-  "mongodb+srv://chirag:6398356528@cluster0.gha33.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// below line is added only in same folder only
+dotenv.config({ path: "./config.env" });
+require("./db/conn");
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log("mongo connected");
-  })
-  .catch((err) => {
-    console.log("mongoo connection failed");
-  });
+const PORT = process.env.PORT;
 
-// database ends
 // Middleware
 
 const middleware = (req, res, next) => {
@@ -52,6 +39,6 @@ app.get("/signup", (req, res) => {
   res.send("hello from register");
 });
 // routes end
-app.listen(3000, () => {
-  console.log("server is running at port 3000");
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
 });
