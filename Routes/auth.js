031377c1d6700e3dcbf43502ceefc366b3eb4726
Chirag <agarwalchirag112@gmail.com>
2021-04-05 13:08:42 +0530

    added code to save data to our database in auth middleware

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const router = express.Router();
+require("../db/conn");
+const User = require("../model/userSchema");
 
 router.get("/", (req, res) => {
   res.send("i am from auth file");
 });
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "plz filled all fields" });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "email already exist" });
+      }
+      const user = new User({ name, email, phone, work, password, cpassword });
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "user registered successfully" });
+        })
+        .catch((err) =>
+          res.status(500).json({ error: "failed registeration" })
+        );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 module.exports = router;
