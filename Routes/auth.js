7d07db65d551ca761744d55fa198d98c9b4341dd
Chirag <agarwalchirag112@gmail.com>
2021-04-06 11:13:35 +0530

    make password secure implement hashing using bcryptjs

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,34 +3,76 @@ const router = express.Router();
 require("../db/conn");
 const User = require("../model/userSchema");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.send("i am from auth file");
 });
 
-router.post("/register", (req, res) => {
-  const { name, email, phone, work, password, cpassword } = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, phone, work, password, cpassword } = req.body;
 
-  if (!name || !email || !phone || !work || !password || !cpassword) {
-    return res.status(422).json({ error: "plz filled all fields" });
-  }
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+      return res.status(422).json({ error: "plz filled all fields" });
+    }
+    userExist = await User.findOne({ email: email });
 
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "email already exist" });
+    if (userExist) {
+      return res.status(422).json({ error: "email already exist" });
+    } else if (cpassword !== password) {
+      return res.status(422).json({ error: "password is not matching" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword }); //creation of new document in collection users
+
+      const userRegister = await user.save();
+
+      if (userRegister) {
+        res.status(201).json({ message: "register successfully" });
+      } else {
+        res.status(500).json({ error: "unsuccessful register" });
       }
-      const user = new User({ name, email, phone, work, password, cpassword });
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "user registered successfully" });
-        })
-        .catch((err) =>
-          res.status(500).json({ error: "failed registeration" })
-        );
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(422).json({ error: "plz fill all field" });
+    }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      console.log(userExist);
+    } else {
+      console.log("invalid email and password");
+      res.status(422).json({ errror: "user not exist" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 module.exports = router;
+
+// using promises
+// User.findOne({ email: email })
+//   .then((userExist) => {
+//     if (userExist) {
+//       return res.status(422).json({ error: "email already exist" });
+//     }
+//     const user = new User({ name, email, phone, work, password, cpassword }); //creation of new document in collection users
+//     user
+//       .save()
+//       .then(() => {
+//         res.status(201).json({ message: "user registered successfully" });
+//       })
+//       .catch((err) =>
+//         res.status(500).json({ error: "failed registeration" })
+//       );
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// using async await
--- model/userSchema.js
+++ model/userSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -18,13 +18,21 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
   work: {
-    type: string,
+    type: String,
     required: true,
   },
   cpassword: {
-    type: string,
+    type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.password, 12);
+  }
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
