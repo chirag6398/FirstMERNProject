5dd91ea6ec8508f7145258b0d18ac0a669907576
Chirag <agarwalchirag112@gmail.com>
2021-04-30 18:23:15 +0530

    rearrange code

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ dotenv.config({ path: "./config.env" });
 require("./db/conn");
 
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 // Middleware
 app.use(express.json());
@@ -27,6 +20,13 @@ app.use(require("./Routes/auth"));
 // app.get("/signup", (req, res) => {
 //   res.send("hello from register");
 // });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
