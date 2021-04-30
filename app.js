18182173e04ba43e85550ab96f5845b78bdd0cd0
Chirag <agarwalchirag112@gmail.com>
2021-04-30 18:21:27 +0530

    resolve type error

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ require("./db/conn");
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
 }
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Middleware
 app.use(express.json());
