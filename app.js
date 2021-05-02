e798a09a1016d3e14788bb91bef51dfec3265743
Chirag <agarwalchirag112@gmail.com>
2021-05-02 18:48:18 +0530

    type  error

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(require("./Routes/auth"));
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.use(express.static(path.join(__dirname, "client/build"));
- 
+  app.use(express.static(path.join(__dirname, "client/build")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
