9b4845fbe93add206b3990b3af7d498e662de346
Chirag <agarwalchirag112@gmail.com>
2021-05-04 10:34:31 +0530

    making responsive design

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../App";
 
 export default function Nav() {
-  const { state} = useContext(UserContext);
+  const { state } = useContext(UserContext);
   return (
-    <div className="nav__container" style={{backgroundColor:"#004643"}}>
+    <div className="nav__container" style={{ backgroundColor: "#004643" }}>
       <nav className="navbar navbar-expand-lg navbar-light ">
         <Link className="navbar-brand" to="/">
-          <h3 style={{color:"#fffffe",letterSpacing:"3px"}}>MERN</h3>
+          <h3 style={{ color: "#fffffe", letterSpacing: "3px" }}>MERN</h3>
         </Link>
         <button
           className="navbar-toggler"
@@ -23,39 +23,90 @@ export default function Nav() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav" >
-          <ul className="navbar-nav justify-content-end align__right" >
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav justify-content-end align__right">
             <li className="nav-item active">
-              <Link className="nav-link"  to="/" style={{color:"#fffffe",fontWeight:"600",letterSpacing:"1px"}}>
+              <Link
+                className="nav-link"
+                to="/"
+                style={{
+                  color: "#fffffe",
+                  fontWeight: "600",
+                  letterSpacing: "1px",
+                }}
+              >
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" style={{color:"#fffffe",fontWeight:"600",letterSpacing:"1px"}} to="/about">
+              <Link
+                className="nav-link"
+                style={{
+                  color: "#fffffe",
+                  fontWeight: "600",
+                  letterSpacing: "1px",
+                }}
+                to="/about"
+              >
                 About
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" style={{color:"#fffffe",fontWeight:"600",letterSpacing:"1px"}} to="/contact">
+              <Link
+                className="nav-link"
+                style={{
+                  color: "#fffffe",
+                  fontWeight: "600",
+                  letterSpacing: "1px",
+                }}
+                to="/contact"
+              >
                 Contact
               </Link>
             </li>
             {!state ? (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link " style={{color:"#fffffe",fontWeight:"600",letterSpacing:"1px"}} to="/login" tabIndex="-1">
+                  <Link
+                    className="nav-link "
+                    style={{
+                      color: "#fffffe",
+                      fontWeight: "600",
+                      letterSpacing: "1px",
+                    }}
+                    to="/login"
+                    tabIndex="-1"
+                  >
                     Login
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link " style={{color:"#fffffe",fontWeight:"600",letterSpacing:"1px"}} to="/register" tabIndex="-1">
+                  <Link
+                    className="nav-link "
+                    style={{
+                      color: "#fffffe",
+                      fontWeight: "600",
+                      letterSpacing: "1px",
+                    }}
+                    to="/register"
+                    tabIndex="-1"
+                  >
                     Register
                   </Link>
                 </li>
               </>
             ) : (
               <li className="nav-item">
-                <Link className="nav-link " style={{color:"#fffffe",fontWeight:"600",letterSpacing:"1px"}} to="/logout" tabIndex="-1">
+                <Link
+                  className="nav-link "
+                  style={{
+                    color: "#fffffe",
+                    fontWeight: "600",
+                    letterSpacing: "1px",
+                  }}
+                  to="/logout"
+                  tabIndex="-1"
+                >
                   logout
                 </Link>
               </li>
